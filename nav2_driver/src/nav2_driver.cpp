cb12763b1e7c30c18a4584bea99a7b55c47660e6
agentx3r <paul@bovbel.com>
2014-04-15 15:30:03 -0400

    initial dir structure

---
+++ nav2_driver/src/nav2_driver.cpp
@@ -0,0 +1,272 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+
+#include <nav2remote/nav2remote.h>
+
+#include <boost/interprocess/smart_ptr/shared_ptr.hpp>
+
+namespace nav2_driver{
+
+class Nav2Driver{
+
+public:
+
+    Nav2Driver(std::string name) :
+        nh_(),
+        private_nh_("~")
+    {
+
+        //get robot address and port
+        private_nh_.param<std::string>("robot_address", robot_address_, "");
+        if(robot_address_.empty()){
+            std::string message = "Please provide address for Nav2";
+            ROS_ERROR_STREAM(message);
+            throw std::runtime_error(message);
+        }
+        private_nh_.param<int>("robot_port", robot_port_, 5010);
+
+        //get parameter for unique tf names
+        std::string robot_name;
+        private_nh_.param<std::string>("robot_name", robot_name, "");
+        robot_prefix_ = robot_name + "_";
+        //get parameter for inverted odometry (for use with robot_pose_ekf)
+        private_nh_.param<bool>("invert_odom", invert_odom_, false);
+
+        connect();
+
+        odom_pub_ = nh_.advertise<nav_msgs::Odometry>("odom", 10);
+        odom_loop_ = nh_.createTimer(ros::Duration(0.1), boost::bind(&Nav2Driver::publishOdometry, this, invert_odom_));
+        cmd_sub_ = nh_.subscribe("cmd_vel", 5, &Nav2Driver::setVelocity, this);
+    }
+
+    ~Nav2Driver(){
+        ROS_INFO("Disconnecting from Casper base");
+    }
+
+protected:
+
+    bool connect(){
+
+        if(remote_){
+            ROS_INFO("Resetting connection to Casper Nav2 base");
+
+            //drop base connection
+            remote_.reset();
+
+            //save odometry offset for new base connection odometry
+            Pose2D offset = odom_measurement_.getDist();
+            odom_measurement_ = OdometryMeasurement(offset);
+        }
+
+        for(int retry = 0; retry < 5; retry++){
+            try{
+                remote_ = boost::shared_ptr<Nav2Remote>(new Nav2Remote(robot_address_.c_str(),robot_port_));
+                ROS_INFO_STREAM("Connected to Nav2 base on " << robot_address_ <<":"<< robot_port_);
+                return true;
+            }catch(std::exception& e){
+                ROS_ERROR_STREAM(e.what());
+                ros::Duration(0.2).sleep();
+            }
+        }
+
+        //TODO
+        ROS_ERROR_STREAM("Failed to connect to Nav2 base on " << robot_address_ <<":"<< robot_port_);
+        throw std::runtime_error("Failed");
+
+    }
+
+    void publishOdometry(bool invert_odom){
+
+        //absolute robot position according to nav2
+        double rx, ry, rth;
+
+        Pose2D meas;
+
+        //reset the connection until command succeeds
+        while(!remote_ || remote_->estimatePosition(meas.x, meas.y, meas.th) < 0){
+            connect();
+        }
+
+        odom_measurement_.updateWithAbsolute(meas);
+        tf_broadcaster_.sendTransform(odom_measurement_.getTransform(invert_odom_,robot_prefix_));
+        odom_pub_.publish(odom_measurement_.getMessage(robot_prefix_));
+
+    }
+
+    void setVelocity(const geometry_msgs::TwistConstPtr& twist){
+
+        //convert from vector velocity to relative angular velocity
+        double vs = sqrt(pow(twist->linear.x,2) + pow(twist->linear.y,2));
+        double vn = atan2(twist->linear.y,twist->linear.x) * (180 / M_PI);
+
+        //reset the connection until command succeeds
+        while(!remote_ || remote_->setRelativeVelocity(vn, vs, twist->angular.z) < 0){
+            connect();
+        }
+    }
+
+private:
+
+    struct Pose2D{
+
+        Pose2D() : x(0), y(0), th(0) {}
+        Pose2D(double x, double y, double th) : x(x), y(y), th(th) {}
+        double x, y, th;
+        Pose2D& operator +=(Pose2D const& other) {
+            x += other.x;
+            y += other.y;
+            th += other.th;
+            return *this;
+        }
+        Pose2D& operator -=(Pose2D const& other) {
+            x -= other.x;
+            y -= other.y;
+
+            //detect rollover
+            if(abs(th - other.th) > M_PI){
+                if(other.th>0){
+                    th += 2*M_PI;
+                }else{
+                    th -= 2*M_PI;
+                }
+            }
+            th -= other.th;
+            return *this;
+        }
+        Pose2D& operator /=(double const& other) {
+            x /= other;
+            y /= other;
+            th /= other;
+            return *this;
+        }
+        inline Pose2D& operator +(const Pose2D& other) { return *this += other; }
+        inline Pose2D& operator -(const Pose2D& other) { return *this -= other; }
+        inline Pose2D& operator /(const double& other) { return *this /= other; }
+
+
+    };
+
+    class OdometryMeasurement{
+
+    public:
+
+        OdometryMeasurement(): offset_(), last_time_(ros::Time::now()) {}
+
+        OdometryMeasurement(Pose2D offset): offset_(offset), last_time_(ros::Time::now()) {}
+
+        void updateWithAbsolute(Pose2D abs){
+            updateWithRelative(abs - prev_);
+            prev_ = abs;
+        }
+
+        void updateWithRelative(Pose2D delta){
+            double elapsed = (ros::Time::now() - last_time_).toSec();
+            last_time_ = ros::Time::now();
+
+            dist_ += delta;
+            vel_ = delta / elapsed;
+
+        }
+
+        geometry_msgs::TransformStamped getTransform(bool invert_odom, std::string robot_prefix){
+
+            //build transform message
+            geometry_msgs::TransformStamped transform;
+            transform.header.stamp = last_time_;
+
+            //invert odometry if necessary
+            tf::Transform temp = tf::Transform(tf::createQuaternionFromYaw(dist_.th + offset_.th), tf::Vector3(dist_.x + offset_.x, dist_.y + offset_.y, 0));
+            if(invert_odom){
+                temp = temp.inverse();
+                transform.header.frame_id = robot_prefix + "base_footprint";
+                transform.child_frame_id = robot_prefix + "odom";
+            }else{
+                transform.header.frame_id = robot_prefix + "odom";
+                transform.child_frame_id = robot_prefix + "base_footprint";
+            }
+            tf::transformTFToMsg(temp, transform.transform);
+
+            return transform;
+
+        }
+
+
+        nav_msgs::Odometry getMessage(std::string robot_prefix){
+
+            //build odometry message
+            nav_msgs::Odometry message;
+
+            message.header.stamp = last_time_;
+            message.header.frame_id = robot_prefix + "odom";
+            message.child_frame_id = robot_prefix + "base_link";
+            message.pose.pose.position.x = dist_.x + offset_.x;
+            message.pose.pose.position.y = dist_.y + offset_.y;
+            message.pose.pose.position.z = 0.0;
+            message.pose.pose.orientation = tf::createQuaternionMsgFromYaw(dist_.th + offset_.th);
+            message.pose.covariance[0] = 10^-3;
+            message.pose.covariance[7] = 10^-3;
+            message.pose.covariance[14] = 10^6;
+            message.pose.covariance[21] = 10^6;
+            message.pose.covariance[28] = 10^6;
+            message.pose.covariance[35] = 10^3;
+
+            message.twist.twist.linear.x = vel_.x;
+            message.twist.twist.linear.y = vel_.y;
+            message.twist.twist.angular.z = vel_.th;
+            message.twist.covariance[0] = 10^-3;
+            message.twist.covariance[7] = 10^-3;
+            message.twist.covariance[14] = 10^6;
+            message.twist.covariance[21] = 10^6;
+            message.twist.covariance[28] = 10^6;
+            message.twist.covariance[35] = 10^3;
+
+            return message;
+
+        }
+
+        Pose2D getDist(){
+            return dist_;
+        }
+
+    private:
+
+        Pose2D dist_, vel_, prev_, offset_;
+        ros::Time last_time_;
+
+    };
+
+private:
+
+    ros::NodeHandle nh_;
+    ros::NodeHandle private_nh_;
+    tf::TransformBroadcaster tf_broadcaster_;
+
+    boost::shared_ptr<Nav2Remote> remote_;
+
+    ros::Publisher odom_pub_;
+    ros::Subscriber cmd_sub_;
+    ros::Timer odom_loop_;
+    OdometryMeasurement odom_measurement_;
+
+    std::string robot_address_, robot_prefix_;
+    int robot_port_;
+    bool invert_odom_;
+
+};
+
+}
+
+int main(int argc, char **argv) {
+
+    ros::init(argc, argv, "nav2_driver");
+    nav2_driver::Nav2Driver nav2_driver(ros::this_node::getName());
+    ros::spin();
+
+    return 0;
+}
+
+
+
